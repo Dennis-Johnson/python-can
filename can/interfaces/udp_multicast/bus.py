5d604ae5aeea75b80cf20887d2aead0a3ee7d78f
Dennis Johnson <densarjohn@gmail.com>
2025-05-19 14:48:57 +0530

    disable loopback for ipv4

--- can/interfaces/udp_multicast/bus.py
+++ can/interfaces/udp_multicast/bus.py
@@ -282,6 +282,12 @@ class GeneralPurposeUdpMulticastBus:
             if hasattr(socket, "SO_REUSEPORT"):
                 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
 
+            # if receive_own_messages : raise NotImplementedError
+            # enable_loop = 1 is receive_own_messages else 0
+            # sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_LOOP, 0) for ipv6. 
+            sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_LOOP, 0)
+            
+
             # set how to receive timestamps
             try:
                 sock.setsockopt(socket.SOL_SOCKET, SO_TIMESTAMPNS, 1)
